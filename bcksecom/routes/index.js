1504c171289e1344eef4ff453897cea0e5177781
Ricardo García <rjozze10@gmail.com>
2020-10-01 22:55:39 -0600

    clase 20200924

--- bcksecom/routes/index.js
+++ bcksecom/routes/index.js
@@ -6,4 +6,31 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get(
+  '/info', 
+  function(req,res){
+    var infoObj = {}; // $infoObj = array()
+    infoObj.cuenta = 1508199500157;
+    infoObj.nombre = "Ricardo Garcia";
+    infoObj.email = "rjozze10@gmail.com";
+    res.json(infoObj);
+  }
+); //get info
+
+router.get(
+  '/hook', 
+  function(req, res){
+    res.render("hook", {})
+  }
+  ); //get hook
+
+router.post(
+  '/hook', 
+  function(req, res){
+    var body = req.body;
+    console.log(body);
+    res.render("hook", body);
+  }
+  ); //post hook
+
 module.exports = router;
